9de9f3169136c820801a0b044b02306552088eb3
beaPaludetto <beatrizpalubarreto@gmail.com>
2024-06-28 10:29:25 -0300

    Estilizando e arrumando meu sistema

--- 27.06.2024/sistema-aluno/script.js
+++ 27.06.2024/sistema-aluno/script.js
@@ -1,16 +1,32 @@
 console.log("bem vindo!");
 const button = document.querySelector("button");
 const inputNota= document.querySelector("#nota");
+// criando uma função para limpar o campo
+function clearForm() {
+    inputNota.value = "";
+}
 
 // criando uma função
 button.addEventListener("click", function(event) {
     event.preventDefault();
-    console.log(inputNota.value);
+    const inputNotaValue = inputNota.value 
     
-    if(inputNota.value <= 6) {
+   //validação de formulário
+   if(inputNotaValue == ''){
+    alert("Campo obrigatório!");
+    return false;
+   }
+   if(inputNotaValue){
+
+   }
+   
+    // logica
+    if(inputNotaValue <= 6) {
         alert("reprovado");
     }else{
-        alert("aprovadissimo")
-    };
+        alert("aprovadissimo");
+    }
+  //chamando função de limpar campo
+  clearForm();
 
 });
